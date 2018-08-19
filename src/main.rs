097c1849d5bc77da1afe4bdd8551ab782fc06c3e
Michael Auracher <michael.auracher@gmail.com>
2018-08-19 19:09:24 +0200

    Same functionality as before but now with environment usage.

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,9 @@ const OP_TAIL: &str = "tail";
 const OP_JOIN: &str = "join";
 const OP_EVAL: &str = "eval";
 
-type Env<'a, 'b> = HashMap<&'a str, Ast<'b>>;
-type LFunction<'a> = fn(Ast<'a>, Env) -> Ast<'a>;
+type Env = HashMap<String, Ast>;
+type OwnlispResult = Result<Ast, failure::Error>;
+type LFunction = fn(Ast, &mut Env) -> OwnlispResult;
 
 mod editline {
   use std::io::{self, Write};
@@ -61,16 +62,26 @@ const _GRAMMAR: &str = include_str!("ownlisp.pest");
 #[grammar = "ownlisp.pest"]
 struct OwnlispParser;
 
-#[derive(Debug)]
-enum Ast<'a> {
+#[derive(Clone)]
+enum Ast {
   Number(i64),
-  Symbol(&'a str),
-  SExpression(VecDeque<Ast<'a>>),
-  QExpression(VecDeque<Ast<'a>>),
+  Function(LFunction),
+  Symbol(String),
+  SExpression(VecDeque<Ast>),
+  QExpression(VecDeque<Ast>),
   EmptyProgram,
 }
 
-impl<'a> Ast<'a> {
+impl std::fmt::Debug for Ast {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    match *self {
+      Ast::Function(_) => write!(f, "Function(<function>)"),
+      _ => write!(f, "{:?}", self),
+    }
+  }
+}
+
+impl Ast {
   fn is_qexpression(&self) -> bool {
     match self {
       Ast::QExpression(_) => true,
@@ -86,7 +97,7 @@ impl<'a> Ast<'a> {
   }
 }
 
-impl<'a> std::fmt::Display for Ast<'a> {
+impl std::fmt::Display for Ast {
   fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
     match self {
       Ast::Number(num) => write!(f, "{}", num),
@@ -104,6 +115,7 @@ impl<'a> std::fmt::Display for Ast<'a> {
         res = res.and(write!(f, ")"));
         res
       }
+      Ast::Function(_) => write!(f, "<function>"),
       Ast::QExpression(values) => {
         let mut res = write!(f, "{{");
         let mut iter = values.into_iter().peekable();
@@ -122,7 +134,7 @@ impl<'a> std::fmt::Display for Ast<'a> {
   }
 }
 
-fn apply_op<'a>(op: &str, x: Ast<'a>, y: Ast<'a>) -> Result<Ast<'a>, failure::Error> {
+fn apply_op(op: &str, x: Ast, y: Ast) -> OwnlispResult {
   match (x, y) {
     (Ast::Number(x), Ast::Number(y)) => {
       match op {
@@ -161,7 +173,7 @@ fn build_custom_ast(pair: pest::iterators::Pair<Rule>) -> Ast {
       //TODO die ganze Sache mit operator usw muss hier weg und ein flexibles symbol
       //management muss her
       Rule::symbol => {
-        Ast::Symbol(pair.as_str())
+        Ast::Symbol(pair.as_str().to_owned())
       }
 
       Rule::sexpression => {
@@ -195,9 +207,72 @@ fn build_custom_ast(pair: pest::iterators::Pair<Rule>) -> Ast {
   build_ast(pair)
 }
 
-fn evaluate_math_builtin<'a>(op: &str, sexp: VecDeque<Ast<'a>>, env: &mut Env) -> Result<Ast<'a>, failure::Error> {
-  let sexp: Result<VecDeque<Ast>, failure::Error> = sexp.into_iter().map(|exp| evaluate(exp, env)).collect();
-  let sexp = sexp?;
+//TODO: It's functional but needs a lot of cleanup. Maybe some Macros just to train it
+fn evaluate_mult(ast: Ast, env: &mut Env) -> OwnlispResult {
+  if let Ast::SExpression(sexp) = ast {
+    evaluate_math_builtin(OP_MULT, sexp, env)
+  } else {
+    panic!("Wrong call!");
+  }
+}
+
+fn evaluate_div(ast: Ast, env: &mut Env) -> OwnlispResult {
+  if let Ast::SExpression(sexp) = ast {
+    evaluate_math_builtin(OP_DIV, sexp, env)
+  } else {
+    panic!("Wrong call!");
+  }
+}
+
+fn evaluate_rem(ast: Ast, env: &mut Env) -> OwnlispResult {
+  if let Ast::SExpression(sexp) = ast {
+    evaluate_math_builtin(OP_REM, sexp, env)
+  } else {
+    panic!("Wrong call!");
+  }
+}
+
+fn evaluate_exp(ast: Ast, env: &mut Env) -> OwnlispResult {
+  if let Ast::SExpression(sexp) = ast {
+    evaluate_math_builtin(OP_EXP, sexp, env)
+  } else {
+    panic!("Wrong call!");
+  }
+}
+
+fn evaluate_minus(ast: Ast, env: &mut Env) -> OwnlispResult {
+  if let Ast::SExpression(sexp) = ast {
+    evaluate_math_builtin(OP_MINUS, sexp, env)
+  } else {
+    panic!("Wrong call!");
+  }
+}
+
+fn evaluate_add(ast: Ast, env: &mut Env) -> OwnlispResult {
+  if let Ast::SExpression(sexp) = ast {
+    evaluate_math_builtin(OP_PLUS, sexp, env)
+  } else {
+    panic!("Wrong call!");
+  }
+}
+
+fn evaluate_min<'a>(ast: Ast, env: &mut Env) -> OwnlispResult {
+  if let Ast::SExpression(sexp) = ast {
+    evaluate_math_builtin(OP_MIN, sexp, env)
+  } else {
+    panic!("Wrong call!");
+  }
+}
+
+fn evaluate_max(ast: Ast, env: &mut Env) -> OwnlispResult {
+  if let Ast::SExpression(sexp) = ast {
+    evaluate_math_builtin(OP_MAX, sexp, env)
+  } else {
+    panic!("Wrong call!");
+  }
+}
+
+fn evaluate_math_builtin(op: &str, sexp: VecDeque<Ast>, env: &mut Env) -> OwnlispResult {
   //fast bailout if we don't only have numbers as operands
   if !sexp.iter().all(|ast| ast.is_number()) {
     println!("{:?}", sexp);
@@ -220,7 +295,7 @@ fn evaluate_math_builtin<'a>(op: &str, sexp: VecDeque<Ast<'a>>, env: &mut Env) -
             }
           }
           OP_MIN | OP_MAX => Ok(total),
-          _ => bail!("The operator {} is not an unary operator.", op),
+          _ => bail!("The function {} is not an unary function.", op),
         }
       //We have many operands
       } else {
@@ -230,12 +305,15 @@ fn evaluate_math_builtin<'a>(op: &str, sexp: VecDeque<Ast<'a>>, env: &mut Env) -
         Ok(total)
       }
     }
-    None => bail!("Operators need an operand to work on."),
+    None => bail!("Functions need a arguments to work on."),
   }
 }
 
-fn evaluate_eval<'a>(expr: VecDeque<Ast<'a>>, env: &mut Env) -> Result<Ast<'a>, failure::Error> {
-  let mut expr = expr;
+fn evaluate_eval(ast: Ast, env: &mut Env) -> OwnlispResult {
+  let mut expr = match ast {
+    Ast::SExpression(sexp) => sexp,
+    _ => panic!("Wrong call!")
+  };
   if expr.len() != 1 {
     bail!("Eval only works on a single Q-Expression as argument.")
   } else {
@@ -249,9 +327,11 @@ fn evaluate_eval<'a>(expr: VecDeque<Ast<'a>>, env: &mut Env) -> Result<Ast<'a>, 
   }
 }
 
-fn evaluate_join<'a>(expr: VecDeque<Ast<'a>>, env: &mut Env) -> Result<Ast<'a>, failure::Error> {
-  let expr: Result<VecDeque<Ast>, failure::Error> = expr.into_iter().map(|exp| evaluate(exp, env)).collect();
-  let mut expr = expr?;
+fn evaluate_join(ast: Ast, _env: &mut Env) -> OwnlispResult {
+  let mut expr = match ast {
+    Ast::SExpression(sexp) => sexp,
+    _ => panic!("Wrong call!")
+  };
   if !expr.iter().all(|exp| exp.is_qexpression()) {
     bail!("Join can only work on Q-Expressions.")
   }
@@ -273,8 +353,11 @@ fn evaluate_join<'a>(expr: VecDeque<Ast<'a>>, env: &mut Env) -> Result<Ast<'a>, 
   }
 }
 
-fn evaluate_head<'a>(expr: VecDeque<Ast<'a>>, env: &mut Env) -> Result<Ast<'a>, failure::Error> {
-  let mut expr = expr;
+fn evaluate_head(ast: Ast, env: &mut Env) -> OwnlispResult {
+  let mut expr = match ast {
+    Ast::SExpression(sexp) => sexp,
+    _ => panic!("Wrong call!")
+  };
   if expr.len() == 1 {
     //Get the first element of the inner QExpression.
     let first_expr = expr.pop_front().expect("We checked the size.");
@@ -295,8 +378,11 @@ fn evaluate_head<'a>(expr: VecDeque<Ast<'a>>, env: &mut Env) -> Result<Ast<'a>, 
   }
 }
 
-fn evaluate_tail<'a>(expr: VecDeque<Ast<'a>>, env: &mut Env) -> Result<Ast<'a>, failure::Error> {
-  let mut expr = expr;
+fn evaluate_tail(ast: Ast, env: &mut Env) -> OwnlispResult {
+  let mut expr = match ast {
+    Ast::SExpression(sexp) => sexp,
+    _ => panic!("Wrong call!")
+  };
   if expr.len() == 1 {
     //Get the first element of the inner QExpression.
     let first_expr = expr.pop_front().expect("We checked the size.");
@@ -315,58 +401,76 @@ fn evaluate_tail<'a>(expr: VecDeque<Ast<'a>>, env: &mut Env) -> Result<Ast<'a>, 
   }
 }
 
-fn evaluate_list(expr: VecDeque<Ast>) -> Ast {
-  Ast::QExpression(expr)
+fn evaluate_list(ast: Ast, _env: &mut Env) -> OwnlispResult {
+  if let Ast::SExpression(exprs) = ast {
+    Ok(Ast::QExpression(exprs))
+  } else {
+    panic!("This function can only be called with SExpressions");
+  }
 }
 
-fn evaluate_sexpression<'a>(sexp: VecDeque<Ast<'a>>, env: &mut Env) -> Result<Ast<'a>, failure::Error> {
-  let mut sexp = sexp;
+fn evaluate_sexpression(ast: Ast, env: &mut Env) -> OwnlispResult {
+  let sexp = match ast {
+    Ast::SExpression(sexp) => sexp,
+    _ => panic!("called it wrong!")
+  };
+  //Evaluate all arguments and if we have any errors bail out
+  let mut sexp: VecDeque<_> = sexp.into_iter().map(|exp| evaluate(exp, env)).collect::<Result<_, _>>()?;
   match sexp.pop_front() {
-    //we have an operand at the first position
+    //Our arguments are empty so we evaluate to the empty program
     None => Ok(Ast::EmptyProgram),
-    Some(Ast::Symbol(sym)) => match sym {
-      OP_MAX
-      | OP_MIN
-      | OP_DIV
-      | OP_MULT
-      | OP_PLUS
-      | OP_MINUS
-      | OP_EXP
-      | OP_REM => evaluate_math_builtin(sym, sexp, env),
-      OP_JOIN => evaluate_join(sexp, env),
-      OP_HEAD => evaluate_head(sexp, env),
-      OP_TAIL => evaluate_tail(sexp, env),
-      OP_EVAL => evaluate_eval(sexp, env),
-      OP_LIST => Ok(evaluate_list(sexp)),
-      _ => panic!("not yet implemented!")
-    },
+    //we have a function at the first position
+    Some(Ast::Function(fun)) => {
+      fun(Ast::SExpression(sexp), env)
+    }
 
-    //We have no Operator on the first position that's only okay if
-    //We have no other values in the VecDeque!
+    //We have no function in the first position. That's only okay if
+    //we have only one value
     Some(val) => {
       if sexp.is_empty() {
         evaluate(val, env)
       } else {
-        bail!("The first element of a S-Expression needs to be an operator.")
+        bail!("The first element of a S-Expression needs to be a function.")
       }
     }
   }
 }
 
-fn evaluate<'a>(program: Ast<'a>, environment: &mut Env) -> Result<Ast<'a>, failure::Error> {
+fn evaluate(program: Ast, environment: &mut Env) -> OwnlispResult {
   match program {
     Ast::Function(_) => Ok(program),
     Ast::Number(_) => Ok(program),
-    Ast::Symbol(_) => Ok(program),
-    Ast::SExpression(values) => evaluate_sexpression(values, environment),
+    Ast::Symbol(sym) => {
+      if let Some(ast) = environment.get(&sym) {
+        Ok(ast.clone())
+      } else {
+        Ok(Ast::Symbol(sym))
+      }
+    }
+    Ast::SExpression(_) => evaluate_sexpression(program, environment),
     //don't do anything for Q-Expressions
-    Ast::QExpression(values) => Ok(Ast::QExpression(values)),
-    _ => bail!("Unimplemented evaluation attempted."),
+    Ast::QExpression(_) => Ok(program),
+    Ast::EmptyProgram => Ok(program),
   }
 }
 
-fn fill_environment(env: &mut Env) {
+fn add_builtins(env: &mut Env) {
+  //List functions
+  env.insert(OP_LIST.to_owned(), Ast::Function(evaluate_list));
+  env.insert(OP_HEAD.to_owned(), Ast::Function(evaluate_head));
+  env.insert(OP_TAIL.to_owned(), Ast::Function(evaluate_tail));
+  env.insert(OP_EVAL.to_owned(), Ast::Function(evaluate_eval));
+  env.insert(OP_JOIN.to_owned(), Ast::Function(evaluate_join));
 
+  //Mathematical Functions
+  env.insert(OP_PLUS.to_owned(), Ast::Function(evaluate_add));
+  env.insert(OP_MINUS.to_owned(), Ast::Function(evaluate_minus));
+  env.insert(OP_MULT.to_owned(), Ast::Function(evaluate_mult));
+  env.insert(OP_DIV.to_owned(), Ast::Function(evaluate_div));
+  env.insert(OP_EXP.to_owned(), Ast::Function(evaluate_exp));
+  env.insert(OP_REM.to_owned(), Ast::Function(evaluate_rem));
+  env.insert(OP_MIN.to_owned(), Ast::Function(evaluate_min));
+  env.insert(OP_MAX.to_owned(), Ast::Function(evaluate_max));
 }
 
 fn main() {
@@ -374,14 +478,15 @@ fn main() {
   println!("Press Ctrl+c to Exit\n");
 
   let mut environment = Env::new();
-  fill_environment(&mut environment);
+  add_builtins(&mut environment);
 
   while let Some(line) = editline::readline("Ownlisp>") {
+    let line = line.trim();
     editline::add_history(&line);
     if line == "exit" {
       break;
     }
-    let parsed = OwnlispParser::parse(Rule::program, line.trim());
+    let parsed = OwnlispParser::parse(Rule::program, line);
     if let Ok(mut pairs) = parsed {
       let program = build_custom_ast(pairs.next().unwrap());
       //println!("{}", program);
